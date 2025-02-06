e32edf58dfe0db3fd8818d84d210909c25ac4637
Sher Kadirov <kadirov4707@gmail.com>
2025-02-06 04:34:41 +0200

    number Math functions

--- cs_11_js_intro/index.js
+++ cs_11_js_intro/index.js
@@ -1,1 +1,11 @@
-console.log('Hello, World!');
+let a = 100;
+a /= 3;
+console.log("non-normalized of division 100 on 3", a);
+a = Math.trunc(a);
+console.log("normalized of division 100 on 3", a);
+a = Math.round(30.6);
+console.log("30.6 rounded - ", a);
+
+console.log("30.1 after method floor - ", Math.floor(30.1));
+console.log("30.1 after method ceil - ", Math.ceil(30.1));
+
