54dc6c943b87a322822885550c6842172f61fd02
Sher Kadirov <kadirov4707@gmail.com>
2025-02-09 17:53:48 +0200

    cw

--- cs_11_js_intro/index.js
+++ cs_11_js_intro/index.js
@@ -1,11 +1,33 @@
-let a = 100;
-a /= 3;
-console.log("non-normalized of division 100 on 3", a);
-a = Math.trunc(a);
-console.log("normalized of division 100 on 3", a);
-a = Math.round(30.6);
-console.log("30.6 rounded - ", a);
+// let a = 100;
+// a /= 3;
+// console.log("non-normalized of division 100 on 3", a);
+// a = Math.trunc(a);
+// console.log("normalized of division 100 on 3", a);
+// a = Math.round(30.6);
+// console.log("30.6 rounded - ", a);
 
-console.log("30.1 after method floor - ", Math.floor(30.1));
-console.log("30.1 after method ceil - ", Math.ceil(30.1));
+// console.log("30.1 after method floor - ", Math.floor(30.1));
+// console.log("30.1 after method ceil - ", Math.ceil(30.1));
+
+// let str1 = "2a";
+// let str2 = "-5";
+// console.log(3 + "4 * 5" + 2);
+
+// let str = "true";
+// if (str != false){
+//     console.log(str + 123);
+// }else {
+//     console.log(str + 123);
+// }
+// /********************** */
+// console.log(null - "123");
+/****************** */
+// console.log(true + true)
+
+// if(true - true){
+//     console.log(true);
+    
+// }
+
+console.log('true' * false );
 
---
+++ hw_11_js_intro/index.js
@@ -0,0 +1,33 @@
+function sumDigit(num) {
+    let sum = 0;
+    let digits = num.toString();
+    for (let i = 0; i < digits.length; i++) {
+        sum += parseInt(digits[i]);
+    }
+    return sum;
+}
+
+console.log(sumDigit(123));
+
+
+function sumDigits(num) {
+    console.log(num - 123);
+}
+sumDigits("a123");
+
+function sumDigi(num) {
+    console.log(num - 123);
+}
+sumDigi("123a");
+
+function sumDig(num) {
+    let sum = 0;
+    let array = Math.abs(num).toString();
+    for (let i = 0; i < array.length; i++) {
+         sum += parseInt(array[i]);
+    }
+    return sum;   
+}
+    console.log(sumDig(-123));
+
+    
