76522ff7904c7a3f38e04e5ddcc26d64e9e265b1
Sher Kadirov <kadirov4707@gmail.com>
2025-02-22 07:54:30 +0200

    array hw 14

--- cw_13.jsfunction/index.js
+++ cw_13.jsfunction/index.js
@@ -1,46 +1,45 @@
-let array = ["abc", 23, 52, 1992];
+let array = [2, 4, 6, 8];
 
+function evenNumber(num) {
+    return num % 2 === 0;
+}
 function some(array, fun) {
-    for (let i = 0; i < array.length; i++) {
-        if (fun(array[i], i, array)) {
-            return true;
-        }
+    let index = 0;
+    while (index < array.length && !fun(array[index], index)) {
+        index++;
     }
-    return false;
+    return index < array.length;
 }
 
 function every(array, fun) {
-    for (let i = 0; i < array.length; i++) {
-        if (!fun(array[i], i, array)) {
-            return false;
-        }
+    let index = 0;
+    while (index < array.length && fun(array[index], index)) {
+        index++;
     }
-    return true;
+    return index === array.length;
 }
 
 // Juft sonlarni tekshirish funksiyasi
-function evenNumber(num) {
-    return  num % 2 === 0;
-}
+
+
+// **To‘g‘ri massivni e’lon qilish**
+
 
 console.log(`"some" funksiyasidan foydalanib, juft sonlar uchun tekshirish: 
     massiv: ${array}, funksiya: evenNumber, 
     natija: ${some(array, evenNumber)} - true`);
-     
-     
-
-array = [2, 3, 4];
 
 function elmGreaterIndex(elem, index) {
     return elem > index;
 }
 
-console.log(`"every" funksiyasidan foydalanib, barcha elementlar indeks qiymatlaridan katta ekanligini tekshirish:massiv: ${array},
-            funksiya: elmGreaterIndex,  natija: ${every(array, elmGreaterIndex)} - true`);
-   
+console.log(`"every" funksiyasidan foydalanib, barcha elementlar indeks qiymatlaridan katta ekanligini tekshirish:
+    massiv: ${array}, funksiya: elmGreaterIndex,  
+    natija: ${every(array, elmGreaterIndex)} - true`);
 
 // Yana qo'shimcha testlar:
 console.log(some([1, 3, 5, 7], evenNumber)); // false (hech biri juft emas)
 console.log(some([2, 4, 6, 8], evenNumber)); // true (kamida bitta juft bor)
 console.log(every([2, 4, 6, 8], evenNumber)); // true (hammasi juft)
 console.log(every([1, 2, 3, 4], evenNumber)); // false (hammasi juft emas)
+
